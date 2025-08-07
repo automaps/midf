2e38d6ce874a0bcc4ac6179771aeadc44374fdaf
Christian Heider Nielsen <mrnaah@gmail.com>
2025-08-07 09:39:11 +0200

    Refactors import statements in run_all script
    Updates import statements in the run_all script to use absolute imports instead of relative imports.
    
    This change improves the script's portability and resolves potential import issues when running it from different contexts.

--- samples/point_maps/run_all.py
+++ samples/point_maps/run_all.py
@@ -1,7 +1,7 @@
-from .download_files import download_files
-from .fix_pc_jsons import fix_pc_venue_jsons
-from .imdfify_pc_venue import to_imdf_venue
-from .load_pc_imdf_venues import upload_to_mi
+from download_files import download_files
+from fix_pc_jsons import fix_pc_venue_jsons
+from imdfify_pc_venue import to_imdf_venue
+from load_pc_imdf_venues import upload_to_mi
 
 download_files()
 fix_pc_venue_jsons()
